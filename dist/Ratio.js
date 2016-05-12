48eea67cb3e52cdb58e1fb50783334e13cca071a
Frank van der Hoek <frank.vanderhoek@gmail.com>
2016-05-12 13:13:05 +0200

    Merge PR #9
    corrected the height for Android devices
    
    closes #9

--- dist/Ratio.js
+++ dist/Ratio.js
@@ -4,7 +4,12 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var dimension = require('react-native').Dimensions.get('window');
+var React = require('react-native');
+var dimension = React.Dimensions.get('window');
+// remove the status bar height since the modal view does not cover this area
+if (React.Platform.OS === 'android') {
+  dimension.height = dimension.height - 24;
+}
 
 var Ratio = function () {
   function Ratio() {
